ed54fcb9722bdb89fdd5fc88d01be8dae0b31d00
Jackson Gomes <jgomes@ceulp.edu.br>
2017-08-24 21:30:31 -0300

    Usando cards do bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,11 +14,11 @@ export class AppComponent implements OnInit {
     constructor() {
         this.title = 'Vingadores';
         this.vingadores = [
-            new Vingador(1, 'Capitão América', 'Steve Rogers'),
-            new Vingador(2, 'Viúva Negra', 'Natasha Romanoff'),
-            new Vingador(3, 'Ms. Marvel', 'Carol Danvers'),
-            new Vingador(4, 'Deadpool', 'Wade Wilson'),
-            new Vingador(5, 'Gavião Arqueiro', 'Clint Barton')
+            new Vingador(1, 'Capitão América', 'Steve Rogers', 'https://upload.wikimedia.org/wikipedia/pt/b/bc/Steven_Rogers_%28Earth-616%29.jpg'),
+            new Vingador(2, 'Viúva Negra', 'Natasha Romanoff', 'https://upload.wikimedia.org/wikipedia/pt/1/1c/Black_Widow_Vol_6_12.jpg'),
+            new Vingador(3, 'Ms. Marvel', 'Carol Danvers', ''),
+            new Vingador(4, 'Deadpool', 'Wade Wilson', 'https://upload.wikimedia.org/wikipedia/pt/c/ce/Deadpool_Vol_4_7.jpg'),
+            new Vingador(5, 'Gavião Arqueiro', 'Clint Barton', 'https://upload.wikimedia.org/wikipedia/pt/c/cc/Gavi%C3%A3o_Arqueiro_por_Clint_Langley.JPG', 'https://pt.wikipedia.org/wiki/Gavi%C3%A3o_Arqueiro')
         ];
     }
 
--- src/app/vingador.ts
+++ src/app/vingador.ts
@@ -2,10 +2,14 @@ export class Vingador {
     id: number;
     nome: string;
     pessoa: string;
+    urlDaFoto: string;
+    urlWikipedia: string;
 
-    constructor(id: number, nome: string, pessoa?: string) {
+    constructor(id: number, nome: string, pessoa: string, urlDaFoto: string, urlWikipedia: string = '') {
         this.id = id;
         this.nome = nome;
         this.pessoa = pessoa;
+        this.urlDaFoto = urlDaFoto;
+        this.urlWikipedia = urlWikipedia;
     }
 }
